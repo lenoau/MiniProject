d5479aef159dd50e2bcc14fd06b938d47f8d3670
lenoau <ysj1423y@naver.com>
2024-12-13 17:06:11 +0900

    댓글_up,down추가가

--- src/댓글/Comment.jsx
+++ src/댓글/Comment.jsx
@@ -1,58 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CommentTime from './CommentTime';
 import Up from '../Image/Up.png';
 import Down from '../Image/Down.png';
 
-const styles =  {
-    wrapper: {
-        margin: 8,
-        display: 'flex',
-        flexDirection: "row",
-    },
-    imageContainer: {},
-    image: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-    },
-    contentContainer: {
-        marginLeft: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-    },
-    nameText: {
-        color: 'black',
-        fontsize: 16,
-        fontWeight: 'bold',
-    },
-    commentText: {
-        color: 'black',
-        fontSize: 12,
-    },
-    commentdate: {
-        color: 'gray',
-        fontSize: 10,
-    },
-    commentUD: {
-        marginTop: 'auto',
-        display: 'flex',
-        justifyContent: 'center', 
-        alignItems: 'center',
-    },
-};
-
 export default function Comment(props) {
+
+const [up, setup] = useState(0);
+const [down, setDown] = useState(0);
+
+
   return (
-    <div className='justify-between h-full pt-3 pb-5 border-b-2 border-gray-300 ' style = {styles.wrapper}>
-        <div style = {styles.contentContainer}>
-            <span style = {styles.nameText}>{props.name}</span>
-            <span style = {styles.commentText}>{props.comment}</span>
-            <span style = {styles.commentdate}><CommentTime date={props.day}/></span>
+    <div className='justify-between h-full pt-3 pb-5 border-b-2 border-gray-300 flex'>
+        <div className='flex ml-8 justify-center flex-col'>
+            <span className='text-[18px] text-black font-bold'>{props.name}</span>
+            <span className='text-[15px] text-black'>{props.comment}</span>
+            <span className='text-[12px] text-gray-600'><CommentTime date={props.day}/></span>
         </div>
-        <div className='flex' style={styles.commentUD}>
-            <span><img src={Up} alt='Up'/></span>
-            <span><img src={Down} alt='Down' /></span>
+        <div className='flex mt-auto'>
+            <img src={Up} alt='Up' onClick={() => {setup(up + 1)}}/><span className='mx-3'>{up}</span>
+            <img src={Down} alt='Down' onClick={() => {setDown(down + 1)}}/><span className='mx-3'>{down}</span>
         </div>
     </div>
   );
