7f622571ee972fd5599fdb91cbb831cbe8191e3a
lenoau <ysj1423y@naver.com>
2024-12-24 14:15:07 +0900

    추가_수정

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import Login from './로그인_회원가입/Login';
 import MemberShip from './로그인_회원가입/MemberShip';
 import Main from './메인_웹툰화면/Main';
 import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
-import Title from './컴포넌트List/Title';
 
 export default function App() {
   return (
     <div className='Main'>
-      
       <BrowserRouter>
-      {/* <Title /> */}
         <Routes>
+          <Route path='/' element={<Main />} />
           <Route path='/Login' element={<Login />} />
           <Route path="/Membership" element={<MemberShip />} />
           <Route path='/Board' element={<BoardMain />} /> 
